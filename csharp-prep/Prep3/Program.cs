aa94554f0b68d6f2cbe3cf497009dfe654f64add
Alejandro Malvacias <alejandromalvacias1@gmail.com>
2023-09-20 18:51:25 -0400

    Prep 3, 4 and 5 done!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,36 @@ using System;
 
 class Program
 {
+    private static int answerNumber;
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       Random randomGenerator = new Random();
+       int magicNumber = randomGenerator.Next(1, 100);
+
+
+
+        while (magicNumber != answerNumber){
+
+       Console.WriteLine("What is the magic number?");
+       Console.Write("What is your guess?");
+       answerNumber = int.Parse(Console.ReadLine());
+
+       if (answerNumber == magicNumber){
+            Console.WriteLine("You Guessed it");
+       }
+       else if (answerNumber < magicNumber){
+            Console.WriteLine("Lower");
+
+       }
+       else if(answerNumber > magicNumber){
+            Console.WriteLine("Higher");
+       }
+
+
+
+       }
+
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> NumbersList = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int NumberAnswer = int.Parse(Console.ReadLine());
+
+        NumberAnswer = -1;
+        while (NumberAnswer != 0){
+            Console.Write("Enter a list of numbers, type 0 when finished.");
+            int UserAnswer = int.Parse(Console.ReadLine());
+            NumberAnswer = UserAnswer;
+
+            if (NumberAnswer != 0){
+
+                NumbersList.Add(UserAnswer);
+            }
+
+
+        }
+
+        int sum = 0;
+        foreach (int number in NumbersList){
+
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float) sum) / NumbersList.Count;
+        Console.WriteLine($"The average is: {average}");
+
+
+        int max = NumbersList[0];
+
+        foreach (int number in NumbersList)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
+
     }
 }
